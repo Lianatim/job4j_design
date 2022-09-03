492e3901ddfd768da5d5bc97269216273404b290
Liana <95958723+Lianatim@users.noreply.github.com>
2022-09-04 00:36:01 +0300

    6. Перевернуть связанный список

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -5,7 +5,6 @@ import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
-    //private Node<T> tail;
 
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
@@ -80,7 +79,6 @@ public class ForwardLinked<T> implements Iterable<T> {
     private static class Node<T> {
         T value;
         Node<T> next;
-        Node<T> prev;
 
         public Node(T value, Node<T> next) {
             this.value = value;
