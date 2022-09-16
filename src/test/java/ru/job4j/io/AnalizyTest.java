33bfe7639d51346bf2660ce680f87e3d557ed73e
Liana <95958723+Lianatim@users.noreply.github.com>
2022-09-17 00:17:20 +0300

    3.0. Тестирование IO

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -8,7 +8,30 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class AnalizyTest {
     @Test
-    void drop(@TempDir Path tempDir) throws IOException {
+    void unavailable(@TempDir Path tempDir) throws IOException {
+        File source = tempDir.resolve("logServer.txt").toFile();
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        File target  = tempDir.resolve("logServerResult.txt").toFile();
+        Analizy test = new Analizy();
+        test.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(String.valueOf((new StringBuilder())
+                .append("10:57:01;10:59:01;")
+                .append("11:01:02;11:02:02;"))).isEqualTo(rsl.toString());
+    }
+
+    @Test
+    void unavailableTwo(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("logServer.txt").toFile();
         try (PrintWriter out = new PrintWriter(source)) {
             out.println("200 10:56:01");
@@ -27,4 +50,5 @@ class AnalizyTest {
         }
         assertThat("10:57:01;11:02:02;").isEqualTo(rsl.toString());
     }
+
 }
