1cf80435203130036a0b50b85b65cacbdfbd07c0
Liana <95958723+Lianatim@users.noreply.github.com>
2022-09-20 20:10:17 +0300

    5.2. Архивировать проект

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -12,16 +12,15 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public void packFiles(List<Path> sources, File target) {
-
-        for (Path source : sources) {
-            try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-                zip.putNextEntry(new ZipEntry(String.valueOf(source.getFileName())));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toAbsolutePath().toString()))) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toString()))) {
                     zip.write(out.readAllBytes());
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -36,20 +35,30 @@ public class Zip {
         }
     }
 
+    private void validate(ArgsName argsName) {
+        if (!Files.isDirectory(Paths.get(argsName.get("d")))) {
+            throw new IllegalArgumentException(String.format("Not directory %s", argsName.get("d")));
+        }
+        if (!argsName.get("e").startsWith(".")) {
+            throw new IllegalArgumentException(String.format("Invalid file extension %s", argsName.get("e")));
+        }
+        if (!argsName.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException(String.format("Invalid name %s", argsName.get("o")));
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length != 3) {
             throw new IllegalArgumentException("Arguments is null");
         }
 
         Zip zip = new Zip();
-        ArgsName argsName = ArgsName.of(args);
         zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
         );
-        if (!Files.isDirectory(Paths.get(argsName.get("d")))) {
-            throw new IllegalArgumentException(String.format("Not directory %s", Paths.get(argsName.get("d")).toAbsolutePath()));
-        }
+        ArgsName argsName = ArgsName.of(args);
+        zip.validate(argsName);
         List<Path> sources = new ArrayList<>();
         try {
             sources = Search.search(Paths.get(argsName.get("d")), p -> !p.toFile().getName().endsWith(argsName.get("e")));
