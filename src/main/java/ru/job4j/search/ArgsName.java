10e98e77e4f30d701e9c5b67b6848b20abe0036e
Liana <95958723+Lianatim@users.noreply.github.com>
2022-09-26 00:11:02 +0300

    2. Поиск файлов по критерию

---
+++ src/main/java/ru/job4j/search/ArgsName.java
@@ -0,0 +1,54 @@
+package ru.job4j.search;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("The key doesn't exist");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String arg : args) {
+            validate(arg);
+            String[] pair = arg.split("=", 2);
+            values.put(pair[0].substring(1), pair[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments are empty");
+        }
+        names.parse(args);
+        return names;
+    }
+
+    public static void validate(String arg) {
+        if (!arg.contains("=")) {
+            throw new IllegalArgumentException("The key - value pair " + arg + " must contain the character \"=\"");
+        }
+        if (arg.startsWith("-=") || arg.startsWith("=") || arg.indexOf("=") == arg.length() - 1) {
+            throw new IllegalArgumentException("The key - value pair " + arg + " must contain the key");
+        }
+        if (!arg.startsWith("-")) {
+            throw new IllegalArgumentException("The key - value pair " + arg + " must start with the character \"-\"");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
---
+++ src/main/java/ru/job4j/search/SearchFilesPattern.java
@@ -0,0 +1,68 @@
+package ru.job4j.search;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFilesPattern {
+
+    public static Predicate<Path> searchMask(ArgsName argsName) {
+        Predicate<Path> regex = p -> p.toFile().getName()
+                .matches(argsName.get("n"));
+        Predicate<Path> name = p -> p.toFile().getName()
+                .endsWith(argsName.get("n"));
+        Predicate<Path> mask = p -> p.toFile().getName()
+                .matches(argsName.get("n").replace(".", "[.]")
+                        .replace("*", ".+")
+                        .replace("?", "."));
+
+        return argsName.get("t").equals("name") ? name : argsName.get("t").equals("regex") ? regex : mask;
+    }
+
+    private void saveLog(List<Path> log, ArgsName argsName) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(argsName.get("o"))))) {
+            log.forEach(out::println);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Argument(s) is null");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        validate(argsName);
+        Path start = Paths.get(argsName.get("d"));
+        SearchFilesPattern searchFilesPattern = new SearchFilesPattern();
+        searchFilesPattern.saveLog(search(start, searchMask(argsName)), argsName);
+
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static void validate(ArgsName argsName) {
+        if (!Paths.get(argsName.get("d")).toFile().exists()) {
+            throw new IllegalArgumentException(String.format("Not exists %s", Paths.get(argsName.get("d")).toFile().getAbsoluteFile()));
+        }
+        if (!Paths.get(argsName.get("d")).toFile().isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", Paths.get(argsName.get("d")).toFile().getAbsoluteFile()));
+        }
+        if (argsName.get("n").isEmpty()) {
+            throw new IllegalArgumentException("The pattern is empty");
+        }
+        if (argsName.get("t").isEmpty()) {
+            throw new IllegalArgumentException("The type of pattern is empty");
+        }
+        if (argsName.get("o").isEmpty()) {
+            throw new IllegalArgumentException("The sve file is empty");
+        }
+    }
+}
