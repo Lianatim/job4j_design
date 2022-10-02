2dd14d41be06aab7fa00474a84643bddd37e996f
Liana <95958723+Lianatim@users.noreply.github.com>
2022-10-02 23:40:36 +0300

    3. Фильтры

--- src/main/java/ru/job4j/sql/product.sql
+++ src/main/java/ru/job4j/sql/product.sql
@@ -1,25 +1,25 @@
 create table type(
-id serial primary key,
-name varchar(255)
-);
+ id serial primary key,
+ name varchar(255)
+ );
 
-create table product(
-id serial primary key,
-name varchar(255),
-type_id int references type(id),
-expired_date date,
-price float
-);
+ create table product(
+ id serial primary key,
+ name varchar(255),
+ type_id int references type(id),
+ expired_date date,
+ price float
+ );
 
-insert into type(name)
-values ('мясо'), ('рыба'), ('кисломолочные'), ('выпечка'), ('сладости');
+ insert into type(name)
+ values ('мясо'), ('рыба'), ('кисломолочные'), ('выпечка'), ('сладости');
 
-insert into product(name, type_id, expired_date, price)
-values ('курица', 1, '10-02-2022', 205), ('говядина', 1, '10-02-2022', 305),
-('лосось', 2, '09-28-2022', 633), ('сыр адыгейский', 3, '10-02-2022', 120),
-('сыр моцарелла', 3, '10-03-2022', 220), ('сыр камабер', 3, '09-28-2022', 250),
-('сыр плавленый', 3, '09-29-2022', 120), ('круассан', 4, '09-29-2022', 70),
-('мороженое', 5, '10-02-2022', 120), ('круассан', 4, '09-28-2022', 120);
+ insert into product(name, type_id, expired_date, price)
+ values ('курица', 1, '2022-02-10', 205), ('говядина', 1, '2022-02-10', 305),
+ ('лосось', 2, '2022-09-28', 633), ('сыр адыгейский', 3, '2022-02-10', 120),
+ ('сыр моцарелла', 3, '2022-03-10', 220), ('сыр камабер', 3, '2022-09-28', 250),
+ ('сыр плавленый', 3, '2022-09-29', 120), ('круассан', 4, '2022-09-28', 70),
+ ('мороженое', 5, '2022-02-10', 120), ('круассан', 4, '2022-09-28', 120);
 
 --Написать запрос получение всех продуктов с типом "кисломолочные"
 select t.name,  pr.name
@@ -30,7 +30,7 @@ where t.name = 'кисломолочные';
 select * from product where name like  '%мороженое%';
 
 --Написать запрос, который выводит все продукты, срок годности которых уже истек
-select * from product where expired_date < '10-01-2022';
+select * from product where expired_date < current_date;
 
 --Написать запрос, который выводит самый дорогой продукт.
 --Запрос должен быть универсальный и находить все продукты с максимальной ценой.
@@ -56,11 +56,3 @@ where t.name = 'кисломолочные' or t.name ='выпечка';
 select t.name as "Тип продукта", count(t.name) Количество
 from product pr
 join type t on pr.type_id=t.id
-group by t.name
-having count(t.name) < 2;
-
---Вывести все продукты и их тип.
-select pr.name as "Название продукта", t.name as "Тип продукта",
-pr.expired_date as "Срок годности", pr.price Цена
-from product pr
-join type t on pr.type_id=t.id;
