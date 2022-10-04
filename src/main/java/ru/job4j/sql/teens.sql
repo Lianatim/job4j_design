95ee7a758b88a0860d0c68473e0cd95c63cfa421
Liana <95958723+Lianatim@users.noreply.github.com>
2022-10-04 21:06:33 +0300

    0. Join

--- src/main/java/ru/job4j/sql/teens.sql
+++ src/main/java/ru/job4j/sql/teens.sql
@@ -1,19 +1,14 @@
-create table names (
+create table teens (
 id serial primary key,
-name varchar(255)
+name varchar(15),
+gender varchar(255)
 );
 
-create table genders (
-id serial primary key,
-name varchar(255)
-);
+insert into teens (name, gender)
+values ('Маша', 'Agender '), ('Паша', 'Androgyne'),('Максим', 'Trans Man'),('Даша', 'Cisgender Man'),
+		('Саша', 'Cis Female'), ('Марк', 'Gender Nonconforming');
 
-insert into names (name)
-values ('Маша'), ('Паша'),('Максим'),('Даша'),
-		('Саша'),('Марк'),('Матвей'),('Яна'),('Зоя');
-
-insert into genders(name)
-values ('Agender '), ('Androgyne'),('Cis Female'),('Cisgender Man'),
-		('Gender Nonconforming'),('Trans Man'),('Transgender Male'),('Transmasculine'),('Two-spirit');
-
-select g.name гендер, n.name имя from genders g cross join names n;
+select t1.name, t1.gender,
+t2.name, t2.gender
+from teens t1 cross join teens t2
+where t1.name != t2.name;
