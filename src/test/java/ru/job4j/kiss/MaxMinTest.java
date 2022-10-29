b5572a8cbff19751de2cfa22d594a2622cd00670
Liana <95958723+Lianatim@users.noreply.github.com>
2022-10-29 20:14:05 +0300

    1. Принципы Kiss, Dry и Yagni

---
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.kiss;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class MaxMinTest {
+
+    private Comparator<Integer> comparator;
+
+    @BeforeEach
+    void test() {
+        comparator = Comparator.naturalOrder();
+    }
+
+
+    @Test
+    void whenFindMax() {
+        MaxMin max = new MaxMin();
+        List<Integer> search = Arrays.asList(4, 6, 3, 2, 9, 1);
+        assertThat(max.max(search, comparator)).isEqualTo(9);
+    }
+
+    @Test
+    void whenFindMin() {
+        MaxMin min = new MaxMin();
+        List<Integer> search = Arrays.asList(4, 6, 3, 2, 9, 1);
+        assertThat(min.min(search, comparator)).isEqualTo(1);
+    }
+}
